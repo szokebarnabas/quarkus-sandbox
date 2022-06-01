40edbb5542736eb5e684b9f5eac028b6214c192f
Barnabas Szoke <barnabas.szoke@tesco.com>
2022-06-01 21:49:06 +0200

    add nft module

---
+++ nft/src/main/java/TestSimulation.java
@@ -0,0 +1,10 @@
+import io.gatling.core.scenario.Simulation;
+import io.gatling.core.structure.ScenarioBuilder;
+import io.gatling.http.protocol.HttpProtocolBuilder;
+
+import java.time.Duration;
+
+public class TestSimulation extends Simulation {
+
+
+}
